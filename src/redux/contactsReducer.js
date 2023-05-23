b9dcc9bb6aba27bf7694e1bad50b32ccb7d2281c
Belka-S <belka.srg@gmail.com>
2023-05-23 22:31:37 +0200

    add builder

--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -1,46 +1,45 @@
 import { combineReducers, createReducer } from '@reduxjs/toolkit';
 import * as actions from 'redux/actions';
 
-const items = createReducer([], {
-  // fetch Contacts
-  [actions.fetchContactsFulfilled]: (_, action) => action.payload,
-  // add Contact
-  [actions.addContactFulfilled]: (state, action) => {
-    state.push(action.payload);
-  },
-  // delete Contact
-  [actions.deleteContactFulfilled]: (state, action) =>
-    state.filter(el => el.id !== action.payload),
+const items = createReducer(
+  [],
+  builder => {
+    builder
+      .addCase(actions.fetchContactsFulfilled, (_, action) => action.payload)
+      .addCase(actions.addContactFulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(actions.deleteContactFulfilled, (state, action) =>
+        state.filter(el => el.id !== action.payload)
+      );
+  }
+  //   { [actions.fetchContactsFulfilled]: (_, action) => action.payload,
+  //     [actions.addContactFulfilled]: (state, action) => { state.push(action.payload); },
+  //     [actions.deleteContactFulfilled]: (state, action) => state.filter(el => el.id !== action.payload), }
+);
+
+const isLoading = createReducer(false, builder => {
+  builder
+    .addMatcher(
+      action => action.type.endsWith('/Pending'),
+      () => true
+    )
+    .addMatcher(
+      action => action.type.endsWith('/Fulfilled' || '/Rejected'),
+      () => false
+    );
 });
 
-const isLoading = createReducer(false, {
-  // fetch Contacts
-  [actions.fetchContactsPending]: () => true,
-  [actions.fetchContactsFulfilled]: () => false,
-  [actions.fetchContactsRejected]: () => false,
-  // add Contact
-  [actions.addContactPending]: () => true,
-  [actions.addContactFulfilled]: () => false,
-  [actions.addContactRejected]: () => false,
-  // delete Contact
-  [actions.deleteContactPending]: () => true,
-  [actions.deleteContactFulfilled]: () => false,
-  [actions.deleteContactRejected]: () => false,
-});
-
-const error = createReducer(false, {
-  // fetch Contacts
-  [actions.fetchContactsPending]: () => null,
-  [actions.fetchContactsFulfilled]: () => null,
-  [actions.fetchContactsRejected]: (_, action) => action.payload,
-  // add Contact
-  [actions.addContactPending]: () => null,
-  [actions.addContactFulfilled]: () => null,
-  [actions.addContactRejected]: (_, action) => action.payload,
-  // delete Contact
-  [actions.deleteContactPending]: () => null,
-  [actions.deleteContactFulfilled]: () => null,
-  [actions.deleteContactRejected]: (_, action) => action.payload,
+const error = createReducer(null, builder => {
+  builder
+    .addMatcher(
+      action => action.type.endsWith('/Pending' || '/Fulfilled'),
+      () => null
+    )
+    .addMatcher(
+      action => action.type.endsWith('/Rejected'),
+      (_, action) => action.payload
+    );
 });
 
 export const contactsReducer = combineReducers({ items, isLoading, error });
